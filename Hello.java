e9ef034adb00ae734c8c25a3b8969a26fb456d0c
emmamolenaar <emma.molenaar@hotmail.com>
2025-01-20 16:42:17 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hey yasmijn hoe gaat het");
+        System.out.println("Test test");
     }
 }
